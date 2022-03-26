5643e386b4126e33d651b8f72defae767da52e3a
tristan.stricker <tristan.stricker@gmail.com>
2022-03-26 13:46:02 -0600

    More refactoring

--- BowlingKata/Kata.cs
+++ BowlingKata/Kata.cs
@@ -1,97 +1,121 @@
+namespace BowlingKata;
+
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
-namespace BowlingKata
+public static partial class Kata
 {
-    public static partial class Kata
+    private class Roll
     {
-        private class Roll
+        public Roll() { }
+
+        public Roll(char value, int frame, int index)
         {
-            public Roll() { }
-
-
-            public Roll(char value, int frame, int index)
-            {
-                Value = value;
-                Frame = frame;
-                Index = index;
-                Score = GetScore();
-            }
-
-            public char Value { get; }
-            public int Frame { get; }
-            public int Index { get; }
-
-            public int Score { get; }
-
-            public bool IsStrike => Value == 'X';
-
-            public bool IsSpare => Value == '/';
-
-            private int GetScore()
-            {
-
-                if (Value == 'X')
-                {
-                    return 10;
-                }
-                else if (Value == '/')
-                {
-                    return 0;
-                }
-
-                return Value - '0';
-            }
+            Value = value;
+            Frame = frame;
+            Index = index;
+            Score = GetScore();
         }
 
-        private class Rolls : ReadOnlyCollection<Roll>
+        public char Value { get; }
+        public int Frame { get; }
+        public int Index { get; }
+
+        public int Score { get; }
+
+        public bool IsStrike => Value == 'X';
+
+        public bool IsSpare => Value == '/';
+
+        private int GetScore()
         {
-            public Rolls(IList<Roll> list) : base(list)
+
+            if (Value == 'X')
             {
+                return 10;
+            }
+            else if (Value == '/')
+            {
+                return 0;
             }
 
-            public static Rolls Create(string inputString)
-            {
+            return Value - '0';
+        }
+    }
 
-                var index = 0;
-
-                var frameStrings = inputString.Split(' ')
-                    .Select((f, i) => new { f, Frame = i + 1 });
-
-                var list = new List<Roll>();
-
-                foreach (var f in frameStrings)
-                {
-                    foreach (var r in f.f.ToCharArray())
-                    {
-                        list.Add(new Roll(r, f.Frame, index));
-                        index++;
-                    }
-                }
-
-                return new Rolls(list);
-            }
-
-            public Roll GetPriorRoll(int index) => GetNextRoll(index, inReverse: true);
-
-            public Roll GetNextRoll(int index, bool inReverse = false)
-            {
-                var toIterateOn = inReverse ? this.Reverse() : this;
-
-                var roll = toIterateOn.Where(x => inReverse ? (x.Index < index) : (x.Index > index))
-                    .FirstOrDefault();
-                return roll ?? new Roll();
-            }
+    private class Rolls : ReadOnlyCollection<Roll>
+    {
+        public Rolls(IList<Roll> list) : base(list)
+        {
         }
 
-        public static int BowlingScore(string inputString)
+        public static Rolls Create(string inputString)
+        {
+            var rollIndex = 0;
+
+            var frameStrings = inputString.Split(' ')
+                .Select((f, i) => new { f, Frame = i + 1 });
+
+            var list = new List<Roll>();
+
+            foreach (var f in frameStrings)
+            {
+                foreach (var r in f.f.ToCharArray())
+                {
+                    list.Add(new Roll(r, f.Frame, rollIndex));
+                    rollIndex++;
+                }
+            }
+
+            return new Rolls(list);
+        }
+
+        public IList<Frame> AsFrames()
+        {
+            return this.GroupBy(x => x.Frame).Select(g => new Frame(g.Key, g)).ToList();
+        }
+
+        public Roll GetPriorRoll(int index) => GetNextRoll(index, inReverse: true);
+
+        public Roll GetNextRoll(int index, bool inReverse = false)
+        {
+            var toIterateOn = inReverse ? this.Reverse() : this;
+
+            var roll = toIterateOn.Where(x => inReverse ? (x.Index < index) : (x.Index > index))
+                .FirstOrDefault();
+            return roll ?? new Roll();
+        }
+    }
+
+    private class Frame
+    {
+        private Frame() { }
+
+
+        public Frame(int frameNumber, IEnumerable<Roll> rolls)
+        {
+
+        }        
+    }
+
+    private class Frames : ReadOnlyCollection<Frame>
+    {
+        private readonly Rolls rolls;
+
+        public Frames(IList<Frame> list) : base(list)
+        {
+        }
+
+        public Frames(Rolls rolls) : this(rolls.AsFrames())
+        {
+            this.rolls = rolls;
+        }
+
+        internal int GetScores()
         {
             var score = 0;
-
-            var rolls = Rolls.Create(inputString);
-
             foreach (var roll in rolls)
             {
                 if (roll.IsStrike)
@@ -113,9 +137,7 @@ namespace BowlingKata
                 else if (roll.IsSpare)
                 {
                     var prior = rolls.GetPriorRoll(roll.Index);
-                    var priorScore = prior.Score;
-
-                    score += 10 - priorScore;
+                    score += 10 - prior.Score;
 
                     if (roll.Frame < 10)
                     {
@@ -131,5 +153,12 @@ namespace BowlingKata
 
             return score;
         }
+    }
+
+    public static int BowlingScore(string inputString)
+    {
+        var rolls = Rolls.Create(inputString);
+        var frames = new Frames(rolls);
+        return frames.GetScores();      
     }
 }
