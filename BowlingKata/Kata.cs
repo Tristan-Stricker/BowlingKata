4a180929ca68fbfc1e18a81af02ff5524df52703
tristan.stricker <tristan.stricker@gmail.com>
2022-03-26 15:39:42 -0600

    Final. Neatest.

--- BowlingKata/Kata.cs
+++ BowlingKata/Kata.cs
@@ -1,48 +1,27 @@
 namespace BowlingKata;
 
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
-public static partial class Kata
+public static class Kata
 {
     private class Roll
     {
         public Roll() { }
 
-        public Roll(char value, int frame, int index)
+        public Roll(char value, int frame)
         {
             Value = value;
             Frame = frame;
-            Index = index;
-            Score = GetScore();
         }
 
         public char Value { get; }
         public int Frame { get; }
-        public int Index { get; }
-
-        public int Score { get; }
 
         public bool IsStrike => Value == 'X';
 
         public bool IsSpare => Value == '/';
-
-        private int GetScore()
-        {
-
-            if (Value == 'X')
-            {
-                return 10;
-            }
-            else if (Value == '/')
-            {
-                return 0;
-            }
-
-            return Value - '0';
-        }
     }
 
     private class Rolls : ReadOnlyCollection<Roll>
@@ -64,7 +43,7 @@ public static partial class Kata
             {
                 foreach (var r in f.f.ToCharArray())
                 {
-                    list.Add(new Roll(r, f.Frame, rollIndex));
+                    list.Add(new Roll(r, f.Frame));
                     rollIndex++;
                 }
             }
@@ -72,93 +51,55 @@ public static partial class Kata
             return new Rolls(list);
         }
 
-        public IList<Frame> AsFrames()
+        public LinkedList<Roll> AsLinkedList()
         {
-            return this.GroupBy(x => x.Frame).Select(g => new Frame(g.Key, g)).ToList();
-        }
-
-        public Roll GetPriorRoll(int index) => GetNextRoll(index, inReverse: true);
-
-        public Roll GetNextRoll(int index, bool inReverse = false)
-        {
-            var toIterateOn = inReverse ? this.Reverse() : this;
-
-            var roll = toIterateOn.Where(x => inReverse ? (x.Index < index) : (x.Index > index))
-                .FirstOrDefault();
-            return roll ?? new Roll();
-        }
-    }
-
-    private class Frame
-    {
-        private Frame() { }
-
-
-        public Frame(int frameNumber, IEnumerable<Roll> rolls)
-        {
-
-        }        
-    }
-
-    private class Frames : ReadOnlyCollection<Frame>
-    {
-        private readonly Rolls rolls;
-
-        public Frames(IList<Frame> list) : base(list)
-        {
-        }
-
-        public Frames(Rolls rolls) : this(rolls.AsFrames())
-        {
-            this.rolls = rolls;
-        }
-
-        internal int GetScores()
-        {
-            var score = 0;
-            foreach (var roll in rolls)
-            {
-                if (roll.IsStrike)
-                {
-                    score += roll.Score;
-
-                    if (roll.Frame < 10)
-                    {
-                        var bonus1 = rolls.GetNextRoll(roll.Index);
-                        var bonus2 = rolls.GetNextRoll(roll.Index + 1);
-
-                        var bonus1Score = bonus1.Score;
-                        var bonus2Score = bonus2.Value == '/' ? 10 - bonus1Score : bonus2.Score;
-
-                        score += bonus1Score;
-                        score += bonus2Score;
-                    }
-                }
-                else if (roll.IsSpare)
-                {
-                    var prior = rolls.GetPriorRoll(roll.Index);
-                    score += 10 - prior.Score;
-
-                    if (roll.Frame < 10)
-                    {
-                        var bonus1 = rolls.GetNextRoll(roll.Index);
-                        score += bonus1.Score;
-                    }
-                }
-                else
-                {
-                    score += roll.Score;
-                }
-            }
-
-            return score;
+            return new LinkedList<Roll>(this);
         }
     }
 
     public static int BowlingScore(string inputString)
     {
-        var rolls = Rolls.Create(inputString);
-        var frames = new Frames(rolls);
-        return frames.GetScores();      
+        var rolls = Rolls.Create(inputString).AsLinkedList();
+        var score = 0;
+
+        for (var node = rolls.First; node != null; node = node.Next)
+        {
+            var roll = node.Value;
+            score += Score(node);
+
+            if (roll.IsStrike && roll.Frame < 10)
+            {
+                score += Score(node.Next) + Score(node.Next?.Next);
+            }
+            else if (roll.IsSpare && roll.Frame < 10)
+            {
+                score += Score(node.Next);
+            }
+        }
+
+        return score;
+    }
+
+    private static int Score(LinkedListNode<Roll>? rollNode)
+    {
+        if (rollNode == null)
+        {
+            return 0;
+        }
+
+        var roll = rollNode.Value;
+
+        if (roll.IsStrike)
+        {
+            return 10;
+        }
+        else if (roll.IsSpare)
+        {
+            return 10 - Score(rollNode.Previous);
+        }
+        else
+        {
+            return roll.Value - '0';
+        }
     }
 }
--- BowlingKata/Tests.cs
+++ BowlingKata/Tests.cs
@@ -1,31 +1,28 @@
 ﻿using FluentAssertions;
 using Xunit;
 
-namespace BowlingKata
+namespace BowlingKata;
+
+public class Tests
 {
-    public static partial class Kata
+    [Theory]
+    [InlineData("Perfect", 300, "X X X X X X X X X XXX")]
+    [InlineData("Bumpers", 20, "11 11 11 11 11 11 11 11 11 11")]
+
+    [InlineData("RandomTest", 83, "9/ 24 60 4/ 00 4/ 4/ 3/ 25 12")]
+    [InlineData("RandomTest2", 125, "4/ 7/ 5/ 14 7/ 5/ 6/ 8/ 43 34")]
+    [InlineData("RandomTest3", 126, "52 3/ 72 3/ 6/ 5/ X 54 52 34")]
+    [InlineData("RandomTest4", 148, "X 8/ X 4/ 53 07 8/ 8/ 7/ 53")]
+
+    [InlineData("BasicTest", 115, "00 5/ 4/ 53 33 22 4/ 5/ 45 XXX")]
+    [InlineData("BasicTest2", 150, "5/ 4/ 3/ 2/ 1/ 0/ X 9/ 4/ 8/8")]
+    public void TestAll(string testCase, int expectedScore, string input)
     {
-        public class Tests
-        {
-            [Theory]
-            [InlineData("Perfect", 300, "X X X X X X X X X XXX")]
-            [InlineData("Bumpers", 20, "11 11 11 11 11 11 11 11 11 11")]
+        // act
+        var score = Kata.BowlingScore(input);
 
-            [InlineData("RandomTest", 83, "9/ 24 60 4/ 00 4/ 4/ 3/ 25 12")]
-            [InlineData("RandomTest2", 125, "4/ 7/ 5/ 14 7/ 5/ 6/ 8/ 43 34")]
-            [InlineData("RandomTest3", 126, "52 3/ 72 3/ 6/ 5/ X 54 52 34")]
-            [InlineData("RandomTest4", 148, "X 8/ X 4/ 53 07 8/ 8/ 7/ 53")]
-
-            [InlineData("BasicTest", 115, "00 5/ 4/ 53 33 22 4/ 5/ 45 XXX")]
-            [InlineData("BasicTest2", 150, "5/ 4/ 3/ 2/ 1/ 0/ X 9/ 4/ 8/8")]
-            public void TestAll(string testCase, int expectedScore, string input)
-            {
-                // act
-                var score = Kata.BowlingScore(input);
-
-                // assert
-                score.Should().Be(expectedScore, because: testCase);
-            }
-        }
+        // assert
+        score.Should().Be(expectedScore, because: testCase);
     }
 }
+
